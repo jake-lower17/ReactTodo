2da108d24e5c87bd5979e82529ab99de1761e7ce
Jacob Lower <jake.lower17@gmail.com>
2017-01-27 07:50:37 -0600

    Testing

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -5,9 +5,24 @@ var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
 var TodoList = require('TodoList');
+var Todo = require('Todo');
 
 describe('TodoList', () => {
   it('should exist', () => {
     expect(TodoList).toExist();
+  });
+  it('should render 1 todo for each todo item', () => {
+    var todos = [
+      {
+        id: 1,
+        text: 'Do Something'
+      }, {
+        id: 2,
+        text: 'check mail'
+      }
+    ];
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+    expect(todosComponents.length).toBe(todos.length);
   });
 });
