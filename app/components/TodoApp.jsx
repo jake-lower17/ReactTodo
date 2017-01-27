3588041bfdca7cfef053da646d35a87f2117dbd2
Jacob Lower <jake.lower17@gmail.com>
2017-01-27 13:49:51 -0600

    Checkboxes

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -13,16 +13,20 @@ var TodoApp = React.createClass({
       todos: [
         {
           id: uuid(),
-          text: 'Walk the dog'
+          text: 'Walk the dog',
+          completed: false
         }, {
           id: uuid(),
-          text: 'Clean the yard'
+          text: 'Clean the yard',
+          completed: true
         }, {
           id: uuid(),
-          text: 'Eat food'
+          text: 'Eat food',
+          completed: false
         }, {
           id: uuid(),
-          text: 'Play video Games'
+          text: 'Play video Games',
+          completed: false
         }
       ]
     };
@@ -34,9 +38,24 @@ var TodoApp = React.createClass({
         ...this.state.todos,
         {
           id: uuid(),
-          text: text
+          text: text,
+          completed: false
         }
       ]
+    });
+  },
+
+  handleToggle: function (id) {
+    var updatedTodos = this.state.todos.map((todo) => {
+
+      if (todo.id === id){
+        todo.completed = !todo.completed;
+      }
+
+      return todo;
+    });
+    this.setState({
+      todos: updatedTodos
     });
   },
 
@@ -52,7 +71,7 @@ var TodoApp = React.createClass({
     return (
       <div>
         <TodoSearch onSearch={this.handleSearch}/>
-        <TodoList todos={todos} />
+        <TodoList todos={todos} onToggle={this.handleToggle}/>
         <AddTodo onAddTodo={this.handleAddTodo}/>
       </div>
     );
