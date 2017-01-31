95da8a56c5a6cee8d672eab98e95b3e23bb532a4
Procyon <Procyon>
2017-01-31 16:31:15 -0600

    screen sizes

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -11,7 +11,7 @@ var TodoApp = React.createClass({
       <div>
         <h1 className="page-title">Todo App</h1>
         <div className="row">
-          <div className="column small-centered small-12 medium-6 large-5">
+          <div className="column small-centered small-12 medium-12 large-8">
             <div className="container">
               <TodoSearch/>
               <TodoList/>
