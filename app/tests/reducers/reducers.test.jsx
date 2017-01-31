4499819689b29246df402d5c6d6e054e270964c0
Procyon <Procyon>
2017-01-31 12:54:46 -0600

    Add redux store

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -27,4 +27,44 @@ describe('Reducers', () => {
       expect(res).toEqual(true);
     });
   });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog',
+      };
+
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+    it('should toggle todo', () => {
+      var todos = [
+        {
+          id: '199',
+          text: 'Write test',
+          completed: true,
+          createdAt: 123,
+          completedAt: 150,
+        }, {
+          id: '201',
+          text: 'write todo.test.jsx test',
+          completed: false,
+          createdAt: 1223,
+          completedAt: 15000,
+        },
+      ];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: '199',
+      };
+
+      var res = reducers.todosReducer(df(todos), df(action));
+      console.log(res);
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
+    });
+  });
 });
