590ccd7a40837952cf89de055d6615ea1c16ef56
Your Name <jake.lower17@gmail.com>
2018-06-19 12:33:36 -0500

    Name Change

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -20,7 +20,7 @@ export class TodoApp extends React.Component {
         <div className="page-actions">
           <a href="#" onClick={this.onLogout.bind(this)}>Logout</a>
         </div>
-        <h1 className="page-title">Todo App</h1>
+        <h1 className="page-title">Grocery List</h1>
         <div className="row">
           <div className="column small-centered small-12 medium-12 large-6">
             <div className="container">
